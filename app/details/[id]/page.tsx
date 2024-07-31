bb46fdaa8f04cb26b4b9b6dbb63afb01121ee779
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2024-07-31 23:31:02 +0100

    adding routing to details page

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -1,26 +1,8 @@
-"use client";
-import { recipe1 } from "@/components/sections/morerecipes";
-import { useRouter } from "next/router";
+import React from "react";
+import RecipeDetail from ".";
 
-const RecipeDetail = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const recipeId = Number(id);
-
-  const recipe = recipe1.find((r) => r.id === recipeId);
-  if (!recipe) {
-    return <div>Recipe not found</div>;
-  }
-
-  return (
-    <>
-      <div>
-        {recipe.meal}
-        {recipe.id}
-      </div>
-    </>
-  );
+const page = () => {
+  return <RecipeDetail />;
 };
 
-export default RecipeDetail;
+export default page;
